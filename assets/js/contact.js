5778952e6260b43b59d417360806738836a91cf5
Sean Emerson <emerson.sean.dev@gmail.com>
2022-06-24 00:02:20 -0700

    working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,31 +1,23 @@
+'use strict'
+
+var form = document.getElementById('js-contactForm');
+
 // called by recaptcha, submits the form (but not via browser default)
-function onSubmit(token) {
-  document.getElementById("js-contactForm").submit();
+window.onSubmit = () => {
+  form.submit();
 }
 
+form.addEventListener('submit', (event) => {
+  // stop default submit behaviour
+  event.preventDefault()
+  event.stopPropagation()
+  // add class to display validation status of fields
+  form.classList.add('was-validated')
+  // if form has passed validation
+  if (form.checkValidity()) {
+    // call recaptcha for check and submission
+    grecaptcha.execute();
+    // recaptcha calls onSubmit after check
+  }
+}, false)
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-
-  var form = document.getElementById('js-contactForm');
-
-  // listens for submit event on form
-  form.addEventListener('submit', function (event) {
-    // stop default submit behaviour
-    event.preventDefault()
-    event.stopPropagation()
-    // add class to display validation status of fields
-    form.classList.add('was-validated')
-    // if form has passed validation
-    if (form.checkValidity()) {
-      // call recaptcha for check and submission
-      grecaptcha.execute();
-      // recaptcha calls onSubmit after check
-    }
-
-  }, false)
-
-})()
