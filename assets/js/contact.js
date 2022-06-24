620dd29824878c1362b30e72eb63ebfdfe3ebd97
Sean Emerson <emerson.sean.dev@gmail.com>
2022-06-24 01:03:50 -0700

    in progress

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,36 @@
 'use strict'
 
-var form = document.getElementById('js-contactForm');
+const form = document.getElementById('js-contactForm');
 
 // called by recaptcha, submits the form (but not via browser default)
 window.submitForm = () => {
-  form.submit();
+  // form.submit();
+  // get form data
+  const formData = new FormData(form);
+  // create new xhr instance
+  const xhr = new XMLHttpRequest;
+  // 1. open and configure xhr
+  xhr.open(form.method, form.action)
+  // 2. set headers
+  xhr.setRequestHeader('Accept', 'application/json');
+  // 3. send data
+  xhr.send(formData);
+  // 4. set timeout to 8sec
+  xhr.timeout = 8000
+  // 5. handle timeout
+  xhr.ontimeout = () => {
+    console.error('timeout');
+  }
+  // 6. on finish load
+  xhr.onloadend = () => {
+    // http success
+    if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 300) {
+      console.log('http success');
+    } else { // http fail
+      console.error('http fail');
+    }
+  }
+
 }
 
 form.addEventListener('submit', (event) => {
