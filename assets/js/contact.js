d202b70207dd273fa5f5d9409e0d6de16fc9d0b2
Sean Emerson <emerson.sean.dev@gmail.com>
2022-06-27 19:12:26 -0700

    added spinner functonality

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const form = document.getElementById('js-contactForm');
+const submitButton = document.getElementById('js-submit');
+
+// enable the button when js loads
+submitButton.disabled = false;
+// create loading spinner
+const spinner = document.createElement('div');
+spinner.id = 'js-spinner';
+spinner.classList.add('spinner-border', 'd-none');
+form.appendChild(spinner);
+// create alert for messages
+const alert = document.createElement('div');
+alert.id = 'js-alert';
+alert.classList.add('alert', 'd-none');
+form.appendChild(alert);
 
 // called by recaptcha, submits the form (but not via browser default)
 window.submitForm = () => {
+  // show loading spinner
+  spinner.classList.remove('d-none');
   // form.submit();
   // get form data
   const formData = new FormData(form);
@@ -19,7 +35,7 @@ window.submitForm = () => {
   xhr.timeout = 8000
   // 5. handle timeout
   xhr.ontimeout = () => {
-    console.error('timeout');
+    message(false, 'There has been a connectivity issue, please try again later');
   }
 
   // 6. on finish load
@@ -34,12 +50,38 @@ window.submitForm = () => {
     const response = JSON.parse(xhr.response);
 
     if (response.success) {
-      console.log('json success');
+      message(true, 'Your message has been sent, we will get back to you shortly.')
     } else if (response.error) {
-      console.log(`json error: ${response.error}`);
+      message(false, `Error: ${response.error}`);
     } else {
-      console.log('generic error');
+      message(false, 'Something went wrong, please try again later');
     }
+  }
+
+  const message = (status, statusText) => {
+    // add alert type based on status
+    if (status === true) {
+      alert.classList.add('alert-success')
+    } else {
+      alert.classList.add('alert-danger')
+    }
+    // add text to alert
+    alert.innerHTML = statusText;
+    // hide spinner
+    spinner.classList.add('d-none');
+    // un-hide alert
+    alert.classList.remove('d-none');
+
+    resetForm();
+  }
+
+  const resetForm = () => {
+    // reset form contents
+    form.reset();
+    // reset validation status with css
+    form.classList.remove('was-validated');
+    // reset google recaptcha
+    grecaptcha.reset();
   }
 
   form.addEventListener('submit', (event) => {
